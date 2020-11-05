6f99403abc567a0604836cbe6ba2d06d85f1e4b5
oscar <oscarGradoClase@gmail.com>
2020-11-05 09:59:02 +0100

    subida

--- src/main/java/com/oscar/controller/Controller.java
+++ src/main/java/com/oscar/controller/Controller.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +16,7 @@ import com.oscar.controller.Negocio.Negocio;
  * Servlet implementation class controller
  */
 //no entiendo, preguntar kike
-//@WebServlet("/Controller")
+@WebServlet("/Controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
